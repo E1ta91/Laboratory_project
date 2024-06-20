eabc2ea82c0dcc90c478a33ca631471b1bc1dbfc
elsie <elsietani117@gmail.com>
2024-06-20 22:36:13 +0000

    added changes2

--- js/index.js
+++ js/index.js
@@ -9,8 +9,5 @@ var swiper = new Swiper(".mySwiper", {
       el: ".swiper-pagination",
       clickable: true,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+
   });
